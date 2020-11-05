6c0b4f325707e7cd2cba918605b159004348bd9c
Rafael Ravedutti <rafaelravedutti@gmail.com>
2020-11-05 23:35:49 +0100

    Add dependencies
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- coupling/parse_cpp.py
+++ coupling/parse_cpp.py
@@ -24,13 +24,15 @@ def find_typerefs(node):
 walberla_path = "/home/rzlin/az16ahoq/repositories/walberla"
 walberla_src = f"{walberla_path}/src"
 walberla_build_src = f"{walberla_path}/build/src"
+clang_include_path = "/software/anydsl/llvm_build/lib/clang/7.0.1/include"
+mpi_include_path = "/software/openmpi/4.0.0-llvm/include"
 mesapd_path = f"{walberla_src}/mesa_pd"
-file = "SpringDashpot.h"
+filename = "SpringDashpot.hpp"
 typename = "SpringDashpot"
 index = clang.cindex.Index.create()
 tu = index.parse(
-    f"{mesapd_path}/kernel/{file}",
-    args=["-Wall", f"-I{walberla_src}", f"-I{walberla_build_src}"])
+    f"{mesapd_path}/kernel/{filename}",
+    args=["-Wall", f"-I{walberla_src}", f"-I{walberla_build_src}", f"-I{clang_include_path}", f"-I{mpi_include_path}"])
 
 diagnostics = tu.diagnostics
 for i in range(0, len(diagnostics)):
