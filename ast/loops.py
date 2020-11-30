66c8c82283143c5d9df68a0f1355e8c730b58d9e
Rafael Ravedutti Lucio Machado <rafael.r.ravedutti@fau.de>
2020-12-01 00:01:14 +0100

    Check neighbor cells during force computation
    Signed-off-by: Rafael Ravedutti Lucio Machado <rafael.r.ravedutti@fau.de>

--- ast/loops.py
+++ ast/loops.py
@@ -165,7 +165,10 @@ class NeighborFor():
         cl = self.cell_lists
         for s in For(self.sim, 0, cl.nstencil):
             neigh_cell = cl.particle_cell[self.particle] + cl.stencil[s]
-            for nc in For(self.sim, 0, cl.cell_sizes[neigh_cell]):
-                it = cl.cell_particles[neigh_cell][nc]
-                for _ in Filter(self.sim, Expr.neq(it, self.particle)):
-                    yield it
+            for _ in Filter(self.sim,
+                            Expr.and_op(neigh_cell >= 0,
+                                        neigh_cell <= cl.ncells_all)):
+                for nc in For(self.sim, 0, cl.cell_sizes[neigh_cell]):
+                    it = cl.cell_particles[neigh_cell][nc]
+                    for _ in Filter(self.sim, Expr.neq(it, self.particle)):
+                            yield it
