98a3381a9ffd825fa0fa64336b6adf5a9f688006
Behzad Safaei <iwia103h@alex1.nhr.fau.de>
2025-02-05 15:49:48 +0100

    Add samples for modular P4IRS

---
+++ examples/modular/sd_sample_3_GPU.cu
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <memory>
+#include <cuda_runtime.h>
+
+#include "spring_dashpot.hpp"
+
+void checkCudaError(cudaError_t err, const char* func) {
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA error in %s: %s\n", func, cudaGetErrorString(err));
+        exit(err);
+    }
+}
+
+__global__ void print_position(PairsAccessor ac, int idx){
+    printf("Position [from device] = (%f, %f, %f) \n", ac.getPosition(idx)[0], ac.getPosition(idx)[1], ac.getPosition(idx)[2]);
+}
+
+__global__ void change_gravitational_force(PairsAccessor ac, int idx){
+    printf("Force [from device] before setting = (%f, %f, %f) \n", ac.getForce(idx)[0], ac.getForce(idx)[1], ac.getForce(idx)[2]);
+
+    pairs::Vector3<double> upward_gravity(0.0, 0.0, 2 * ac.getMass(idx) * 9.81); 
+    ac.setForce(idx, ac.getForce(idx) + upward_gravity);
+
+    printf("Force [from device] after setting = (%f, %f, %f) \n", ac.getForce(idx)[0], ac.getForce(idx)[1], ac.getForce(idx)[2]);
+}
+
+int main(int argc, char **argv) {
+
+    auto pairs_sim = std::make_shared<PairsSimulation>();
+    pairs_sim->initialize();
+
+    // Create PairsAccessor after PairsSimulation is initialized
+    auto ac = std::make_shared<PairsAccessor>(pairs_sim.get());
+
+    pairs_sim->set_domain(argc, argv, 0, 0, 0, 1, 1, 1);
+
+    pairs_sim->create_halfspace(0,0,0,  1, 0, 0,     0, 13);
+    pairs_sim->create_halfspace(0,0,0,  0, 1, 0,     0, 13);
+    pairs_sim->create_halfspace(0,0,0,  0, 0, 1,     0, 13);
+    pairs_sim->create_halfspace(1,1,1,  -1, 0, 0,    0, 13);
+    pairs_sim->create_halfspace(1,1,1,  0, -1, 0,    0, 13);
+    pairs_sim->create_halfspace(1,1,1,  0, 0, -1,    0, 13);
+
+    pairs::id_t pUid = pairs_sim->create_sphere(0.6, 0.6, 0.7,      0, 0, 0,  1000, 0.05, 0, 0);
+    pairs::id_t pUid2 = pairs_sim->create_sphere(0.4, 0.4, 0.76,    2, 2, 0,    1000, 0.05, 0, 0);
+
+    MPI_Allreduce(MPI_IN_PLACE, &pUid, 1, MPI_LONG_LONG_INT, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &pUid2, 1, MPI_LONG_LONG_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    auto pIsLocalInMyRank = [&](pairs::id_t uid){return ac->uidToIdxLocal(uid) != ac->getInvalidIdx();};
+
+    pairs_sim->setup_sim();
+
+    pairs_sim->communicate();
+    // PairsAccessor requires an update when particles are communicated 
+    ac->update();
+
+    int num_timesteps = 2000;
+    int vtk_freq = 20;
+    double dt = 1e-3;
+
+    for (int t=0; t<num_timesteps; ++t){
+        // Up-to-date uids might be on host or device. So sync uid before accessing them from host
+        ac->syncUid();
+
+        // Print position of particle pUid
+        //-------------------------------------------------------------------------------------------
+        if(pIsLocalInMyRank(pUid)){
+            std::cout << "Timestep (" << t << "): Particle " << pUid << " is in rank " << pairs_sim->rank() << std::endl;
+            int idx = ac->uidToIdxLocal(pUid);
+
+            // Up-to-date position might be on host or device. So sync position before printing it from host:
+            ac->syncPosition(); 
+            std::cout << "Position [from host] = (" 
+                    << ac->getPosition(idx)[0] << ", "
+                    << ac->getPosition(idx)[1] << ", " 
+                    << ac->getPosition(idx)[2] << ")" << std::endl;
+            
+            // Position is still synced. Print it from device:
+            print_position<<<1,1>>>(*ac, idx);
+            checkCudaError(cudaDeviceSynchronize(), "print_position");
+        }
+
+        // Calculate forces
+        //-------------------------------------------------------------------------------------------
+        pairs_sim->update_cells();
+        pairs_sim->gravity(); 
+        pairs_sim->spring_dashpot(); 
+
+        // Change gravitational force on particle pUid
+        //-------------------------------------------------------------------------------------------
+        ac->syncUid();
+        if(pIsLocalInMyRank(pUid)){
+            std::cout << "Force Timestep (" << t << "): Particle " << pUid << " is in rank " << pairs_sim->rank() << std::endl;
+            int idx = ac->uidToIdxLocal(pUid);
+
+            // Up-to-date force and mass might be on host or device. So sync before accessing them on device:
+            ac->syncForce();
+            ac->syncMass(); 
+
+            // Modify force from device:
+            change_gravitational_force<<<1,1>>>(*ac, idx);
+            checkCudaError(cudaDeviceSynchronize(), "change_gravitational_force");
+
+            // Force on device was modified. So sync force before printing it from host:
+            ac->syncForce();
+            std::cout << "Force [from host] after changing = (" 
+                    << ac->getForce(idx)[0] << ", "
+                    << ac->getForce(idx)[1] << ", " 
+                    << ac->getForce(idx)[2] << ")" << std::endl;
+        }
+
+        // Euler
+        //-------------------------------------------------------------------------------------------
+        pairs_sim->euler(dt);
+        pairs_sim->reset_volatiles(); 
+
+        // Communicate
+        //-------------------------------------------------------------------------------------------
+        pairs_sim->communicate(t);
+        // PairsAccessor requires an update when particles are communicated
+        ac->update();
+
+        pairs_sim->vtk_write("output/dem_sd_local", 0, ac->nlocal(), t, vtk_freq);
+        pairs_sim->vtk_write("output/dem_sd_ghost", ac->nlocal(), ac->size(), t, vtk_freq);
+    }
+
+    pairs_sim->end();
+}
